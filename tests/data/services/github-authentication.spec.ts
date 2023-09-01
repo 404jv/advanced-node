85f5fa6700c9be72381ceb6d67c9cee59e7cae43
João Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2023-09-01 17:31:42 -0300

    change test strategy to use jest-mock-extended

--- tests/data/services/github-authentication.spec.ts
+++ tests/data/services/github-authentication.spec.ts
@@ -2,32 +2,22 @@ import { AuthenticationError } from '@/domain/errors'
 import { LoadGithubTokenByCodeApi } from '@/data/contracts/apis'
 import { GithubAuthenticationService } from '@/data/services'
 
-class LoadGithubTokenByCodeApiSpy implements LoadGithubTokenByCodeApi {
-  code?: string
-  result = undefined
-  callsCount = 0
-
-  async loadTokenByCode (params: LoadGithubTokenByCodeApi.Params): Promise<LoadGithubTokenByCodeApi.Result> {
-    this.code = params.code
-    this.callsCount++
-    return this.result
-  }
-}
+import { mock } from 'jest-mock-extended'
 
 describe('GithubAuthenticationService', () => {
   it('should call LoadGithubTokenByCodeApi with correct params', async () => {
-    const loadGithubTokenByCodeApi = new LoadGithubTokenByCodeApiSpy()
+    const loadGithubTokenByCodeApi = mock<LoadGithubTokenByCodeApi>()
     const sut = new GithubAuthenticationService(loadGithubTokenByCodeApi)
 
     await sut.perform({ code: 'any_code' })
 
-    expect(loadGithubTokenByCodeApi.code).toBe('any_code')
-    expect(loadGithubTokenByCodeApi.callsCount).toBe(1)
+    expect(loadGithubTokenByCodeApi.loadTokenByCode).toHaveBeenCalledWith({ code: 'any_code' })
+    expect(loadGithubTokenByCodeApi.loadTokenByCode).toHaveBeenCalledTimes(1)
   })
 
   it('should return AuthenticationError when LoadGithubTokenByCodeApi returns undefined', async () => {
-    const loadGithubTokenByCodeApi = new LoadGithubTokenByCodeApiSpy()
-    loadGithubTokenByCodeApi.result = undefined
+    const loadGithubTokenByCodeApi = mock<LoadGithubTokenByCodeApi>()
+    loadGithubTokenByCodeApi.loadTokenByCode.mockResolvedValueOnce(undefined)
     const sut = new GithubAuthenticationService(loadGithubTokenByCodeApi)
 
     const authResult = await sut.perform({ code: 'any_code' })
