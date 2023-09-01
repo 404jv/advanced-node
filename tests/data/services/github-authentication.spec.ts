f05260fa56fe141c1d07c66d1260d2f324ebb81e
João Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2023-09-01 17:35:42 -0300

    refactor code

--- tests/data/services/github-authentication.spec.ts
+++ tests/data/services/github-authentication.spec.ts
@@ -2,25 +2,29 @@ import { AuthenticationError } from '@/domain/errors'
 import { LoadGithubTokenByCodeApi } from '@/data/contracts/apis'
 import { GithubAuthenticationService } from '@/data/services'
 
-import { mock } from 'jest-mock-extended'
+import { mock, MockProxy } from 'jest-mock-extended'
 
 describe('GithubAuthenticationService', () => {
+  let loadGithubTokenByCodeApi: MockProxy<LoadGithubTokenByCodeApi>
+  let sut: GithubAuthenticationService
+  const code = 'any_code'
+
+  beforeEach(() => {
+    loadGithubTokenByCodeApi = mock<LoadGithubTokenByCodeApi>()
+    sut = new GithubAuthenticationService(loadGithubTokenByCodeApi)
+  })
+
   it('should call LoadGithubTokenByCodeApi with correct params', async () => {
-    const loadGithubTokenByCodeApi = mock<LoadGithubTokenByCodeApi>()
-    const sut = new GithubAuthenticationService(loadGithubTokenByCodeApi)
+    await sut.perform({ code })
 
-    await sut.perform({ code: 'any_code' })
-
-    expect(loadGithubTokenByCodeApi.loadTokenByCode).toHaveBeenCalledWith({ code: 'any_code' })
+    expect(loadGithubTokenByCodeApi.loadTokenByCode).toHaveBeenCalledWith({ code })
     expect(loadGithubTokenByCodeApi.loadTokenByCode).toHaveBeenCalledTimes(1)
   })
 
   it('should return AuthenticationError when LoadGithubTokenByCodeApi returns undefined', async () => {
-    const loadGithubTokenByCodeApi = mock<LoadGithubTokenByCodeApi>()
     loadGithubTokenByCodeApi.loadTokenByCode.mockResolvedValueOnce(undefined)
-    const sut = new GithubAuthenticationService(loadGithubTokenByCodeApi)
 
-    const authResult = await sut.perform({ code: 'any_code' })
+    const authResult = await sut.perform({ code })
 
     expect(authResult).toEqual(new AuthenticationError())
   })
